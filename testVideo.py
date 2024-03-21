ed58bdde3b1e9d93c1a7e375e65f5f62ff556c72
Maynafiesta <tarikkartal2@hotmail.com>
2024-03-21 18:52:06 +0300

    little little

--- testVideo.py
+++ testVideo.py
@@ -8,29 +8,30 @@ import torch
 from model import TwinLite as net
 import cv2
 import time
+import math
 
-def Run(model,img):
-    img = cv2.resize(img, (640, 360))
+def Run( model,img ):
+    img = cv2.resize( img, ( 640, 360 ) )
     img_rs=img.copy()
 
-    img = img[:, :, ::-1].transpose(2, 0, 1)
-    img = np.ascontiguousarray(img)
-    img=torch.from_numpy(img)
-    img = torch.unsqueeze(img, 0)  # add a batch dimension
-    img=img.cuda().float() / 255.0
+    img = img[:, :, ::-1].transpose( 2, 0, 1 )
+    img = np.ascontiguousarray( img )
+    img = torch.from_numpy( img )
+    img = torch.unsqueeze( img, 0 )  # add a batch dimension
+    img = img.cuda().float() / 255.0
     img = img.cuda()
     with torch.no_grad():
-        img_out = model(img)
+        img_out = model( img )
     x0=img_out[0]
     x1=img_out[1]
 
-    _,da_predict=torch.max(x0, 1)
-    _,ll_predict=torch.max(x1, 1)
+    _,da_predict = torch.max( x0, 1 )
+    _,ll_predict = torch.max( x1, 1 )
 
-    DA = da_predict.byte().cpu().data.numpy()[0]*255
-    LL = ll_predict.byte().cpu().data.numpy()[0]*255
-    img_rs[DA>100]=[255,0,0]
-    img_rs[LL>100]=[0,255,0]
+    DA = da_predict.byte().cpu().data.numpy()[0] * 255
+    LL = ll_predict.byte().cpu().data.numpy()[0] * 255
+    img_rs[DA>100] = [255,0,0]
+    img_rs[LL>100] = [0,255,0]
     
     return img_rs
 
@@ -41,11 +42,6 @@ model = torch.nn.DataParallel(model)
 model = model.cuda()
 model.load_state_dict(torch.load('pretrained/best.pth'))
 model.eval()
-
-# image_list=os.listdir('images')
-# shutil.rmtree('results')
-# os.mkdir('results')
-
 
 UDP_STREAM_URL = "udpsrc port=5000 " + \
     "! application/x-rtp,media=video,clock-rate=90000,encoding-name=H264,payload=96 " + \
@@ -60,7 +56,6 @@ if not cap.isOpened():
     print( "Error: Could not open UDP stream." )
     exit()
 
-
 while True:
     ret, frame = cap.read()
     
@@ -70,9 +65,10 @@ while True:
     
     startTime = time.time()
     frameDetected = Run( model,frame )
-    duration = time.time() - startTime
-    frameTimeTagged = cv2.putText(frameDetected, str( duration ), ( 50, 50 ), cv2.FONT_HERSHEY_SIMPLEX ,  
-                   1, ( 255, 0, 0 ), 1, cv2.LINE_AA)
+    fpsVal = 1.0 / (time.time() - startTime)
+    frameTimeTagged = cv2.putText( frameDetected, str( fpsVal ), 
+                                  ( 50, 50 ), cv2.FONT_HERSHEY_SIMPLEX , 1, 
+                                  ( 255, 0, 0 ), 1, cv2.LINE_AA)
     cv2.imshow( 'Frame', frameTimeTagged )
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -80,9 +76,3 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
-
-
-# for i, imgName in enumerate(image_list):
-#     img = cv2.imread(os.path.join('images',imgName))
-#     img=Run(model,img)
-#     cv2.imwrite(os.path.join('results',imgName),img)
